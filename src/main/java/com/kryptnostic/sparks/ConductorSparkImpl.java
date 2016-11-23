30787d161bb0e8662f8221844ff65de206c34013
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2016-11-22 20:03:27 -0800

    Fix import

--- src/main/java/com/kryptnostic/sparks/ConductorSparkImpl.java
+++ src/main/java/com/kryptnostic/sparks/ConductorSparkImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
