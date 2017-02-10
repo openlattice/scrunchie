62149a377e69a73acd63d8749bc4cd422e16b0aa
Katherine <katherine@kryptnostic.com>
2017-02-09 18:02:35 -0800

    fix import

--- src/test/java/com/kryptnostic/sparks/KindlingElasticsearchTests.java
+++ src/test/java/com/kryptnostic/sparks/KindlingElasticsearchTests.java
@@ -16,9 +16,8 @@ import com.dataloom.edm.internal.EntitySet;
 import com.dataloom.edm.internal.PropertyType;
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-
-import jersey.repackaged.com.google.common.collect.Maps;
 
 public class KindlingElasticsearchTests extends BaseElasticsearchTest {
         
